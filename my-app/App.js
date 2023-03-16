dc9f4325e95d1a12a4a04c1c925d6a82fae85ffc
NANSAd <112457225+hhhooonnngggooorrrzzzuuulll@users.noreply.github.com>
2023-03-16 21:57:24 +0800

    s
    s

---
+++ my-app/App.js
@@ -0,0 +1,61 @@
+import { StatusBar } from "expo-status-bar";
+import { ScrollView } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scroll11}>
+        <View style={styles.v0}>
+          <View style={styles.v1}>
+            <Text>Товч ном</Text>
+          </View>
+          <View style={styles.v2}>
+            <Text>Товч ном</Text>
+          </View>
+          <View style={styles.v3}>
+            <Text>Товч ном</Text>
+          </View>
+          <View style={styles.v4}>
+            <Text>Товч ном</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  scroll11: { backgroundColor: "white" },
+  v0: {
+    backgroundColor: "white",
+    width: 250,
+    height: 100,
+  },
+  v1: {
+    backgroundColor: "aqua",
+    width: 250,
+    height: 100,
+  },
+  v2: {
+    backgroundColor: "red",
+    width: 250,
+    height: 100,
+  },
+  v3: {
+    backgroundColor: "yellow",
+    width: 250,
+    height: 100,
+  },
+  v4: {
+    backgroundColor: "purple",
+    width: 250,
+    height: 100,
+  },
+});
