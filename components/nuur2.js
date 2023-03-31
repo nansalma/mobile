32a768ce07d1f5faa60a4e809693aa2d2a8c1ffc
NANSAd <>
2023-03-31 10:54:55 +0800

    s
    s

--- components/nuur2.js
+++ components/nuur2.js
@@ -1,13 +1,18 @@
 import { View, Text, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import React from "react";
+import Entypo from "@expo/vector-icons/Entypo";
 
 export default function nuur2() {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.v0}>
-          <View style={styles.detail1}></View>
-          <View style={styles.detail2}></View>
+          <View style={styles.detail1}>
+            <Entypo name="chevron-left" size={32} color="white" />
+          </View>
+          <View style={styles.detail2}>
+            <Entypo name="share" size={32} color="white" />
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -18,16 +23,24 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 
-  v0: { backgroundColor: "beige" },
+  v0: {
+    backgroundColor: "black",
+    flexDirection: "row",
+    width: 500,
+    height: 300,
+    marginTop: 20,
+  },
 
   detail1: {
     width: 20,
     height: 20,
     backgroundColor: "gray",
+    borderRadius: 5,
   },
   detail2: {
     width: 20,
     height: 20,
     backgroundColor: "gray",
+    borderRadius: 5,
   },
 });
