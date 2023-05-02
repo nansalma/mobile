ef1cc28528235dac34e9229c00d538fb9685c6de
NANSAd <>
2023-05-02 17:48:34 +0800

    s
    s

--- assigment/App.js
+++ assigment/App.js
@@ -5,6 +5,7 @@ import {
   SafeAreaView,
   StyleSheet,
   StatusBar,
+  Image,
 } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -13,13 +14,23 @@ export default function App() {
     <SafeAreaView style={styles.safest}>
       <ScrollView>
         <View style={styles.header}>
-          <View style={styles.iconback}>
-            <Ionicons name="arrow-back" size={32} color="white" />
-          </View>
-          <View style={styles.hutulbur}>
-            <Text style={styles.hutulburst}>Хөтөлбөр</Text>
-          </View>
+          <Ionicons name="arrow-back" style={styles.iconback} />
+          <Text style={styles.hutulburug}>Хөтөлбөр</Text>
         </View>
+
+        <Image
+          style={styles.zuragsprite}
+          source={require("./source/images/sprite.png")}
+        />
+        <Text style={styles.tailbar}>
+          "Sprite No Sugar" ундааны{"\n"} бөглөөн доторх 9 оронтой кодыг {"\n"}
+          "MMusic" аппликэйшны промо{"\n"} код хэсэгт оруулан 7 хоногийн{"\n"}{" "}
+          үйлчилгээний эрх аваарай.{"\n"}
+          {"\n"} Урамшууллын хугацаанд нэг{"\n"} хэрэглэгч нийт 4 удаа код{" "}
+          {"\n"}ашиглах боломжтой.{"\n"} {"\n"}Урамшуулал: 2023.03.15 - 06.15
+          {"\n"} дуустал үргэлжилнэ.{"\n"}
+          {"\n"} #HeatHappens #StayCool
+        </Text>
       </ScrollView>
     </SafeAreaView>
   );
@@ -33,28 +44,34 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+
   header: {
     width: 500,
     height: 80,
     backgroundColor: "red",
+    flexDirection: "row",
+    alignItems: "flex-end",
   },
   iconback: {
-    width: 80,
-    height: 80,
-    marginLeft: 20,
-    marginTop: 40,
+    color: "white",
+    fontSize: 30,
+    marginLeft: 10,
   },
-  hutulburst: {
-    fontSize: 25,
+  hutulburug: {
+    fontSize: 20,
     color: "white",
     fontStyle: "italic",
+    marginLeft: 120,
   },
-  hutulbur: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 200,
-    height: 20,
-    backgroundColor: "black",
-    marginTop: 30,
+  zuragsprite: {
+    width: "100%",
+    flex: 1,
+    height: 200,
+    resizeMode: "contain",
+    marginTop: 20,
+  },
+  tailbar: {
+    margin: 20,
+    fontSize: 20,
   },
 });
