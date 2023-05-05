ef0474bbc52a41f226635493512de0889455ef61
NANSAd <>
2023-05-05 10:04:05 +0800

    s
    s

---
+++ assigment/source/screen/2dhnuur.js
@@ -0,0 +1,78 @@
+import {
+  View,
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Text,
+} from "react-native";
+import React from "react";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+export default function App() {
+  return (
+    <SafeAreaView style={styles.safest}>
+      <ScrollView>
+        <View style={styles.header}>
+          <SimpleLineIcons
+            name="basket"
+            size={32}
+            color="white"
+            style={{ marginLeft: 300, marginTop: 50 }}
+          />
+          <AntDesign
+            name="gift"
+            size={32}
+            color="white"
+            style={{ marginTop: 50, padding: 25 }}
+          />
+        </View>
+        <View style={styles.hutulburugview}>
+          <Text style={styles.hutulburug}>Хөтөлбөр</Text>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginLeft: 30,
+            marginRight: 30,
+            marginTop: 10,
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              height: 1,
+              backgroundColor: `#dcdcdc`,
+            }}
+          />
+        </View>
+
+        <View style={styles.sprite}></View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  header: {
+    height: 130,
+    backgroundColor: "red",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  hutulburug: {
+    color: "black",
+    marginTop: 10,
+    fontSize: 20,
+  },
+  hutulburugview: {
+    alignItems: "center",
+  },
+});
