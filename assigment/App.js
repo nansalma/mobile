1a25b6cd6adfbf1c8a7aa85f7314604c53e53a21
NANSAd <>
2023-05-09 17:14:51 +0800

    f
    f

--- assigment/App.js
+++ assigment/App.js
@@ -1,78 +1,90 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Ionicons from "react-native-vector-icons/Ionicons";
+import { Ionicons } from "@expo/vector-icons";
+import nuur from "./source/screen/nuur";
+import nuur1 from "./source/screen/2dhnuur";
+import nuur2 from "./source/screen/3dhnuur";
+import nuur3 from "./source/screen/4dhnuur";
+import header from "./source/screen/header";
 
-// Screens
-import nuur from "../assigment/source/screen/nuur";
-import nuur2 from "../assigment/source/screen/2dhnuur";
-import nuur3 from "../assigment/source/screen/3dhnuur";
-import nuur4 from "../assigment/source/screen/4dhnuur";
+const HomeStack = createNativeStackNavigator();
 
-//Screen names
-const homeName = "Home";
-const secondName = "Хөтөлбөр";
-const thirdName = "Нэмэлт оноо";
-const fourName = "Мэдээ";
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="Home"
+    >
+      <HomeStack.Screen name="Home" component={nuur} />
+      <HomeStack.Screen name="list" component={nuur1} />
+      <HomeStack.Screen name="add" component={nuur2} />
+      <HomeStack.Screen name="message" component={nuur3} />
+      <HomeStack.Screen name="header" component={header} />
+    </HomeStack.Navigator>
+  );
+}
 
-const Tab = createBottomTabNavigator();
-
-function MainContainer() {
+function MyTabs() {
+  const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName={homeName}
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
-            let rn = route.name;
-
-            if (rn === homeName) {
+            if (route.name === "Mplus") {
               iconName = focused ? "home" : "home-outline";
-            } else if (rn === secondName) {
+            } else if (route.name === "Хайх") {
               iconName = focused ? "newspaper-outline" : "newspaper-outline";
-            } else if (rn === thirdName) {
+            } else if (route.name === "Миний сан") {
               iconName = focused ? "settings" : "settings-outline";
-            } else if (rn === fourName) {
+            } else if (route.name === "Message") {
               iconName = focused
                 ? "chatbox-ellipses-outline"
                 : "chatbox-ellipses-outline";
             }
 
-            // You can return any component that you like here!
             return <Ionicons name={iconName} size={size} color={color} />;
           },
+          tabBarStyle: {
+            height: 60,
+            paddingHorizontal: 5,
+            paddingTop: 0,
+            backgroundColor: "#4D4D4D",
+            position: "absolute",
+            borderTopWidth: 0,
+          },
+          tabBarActiveTintColor: "blue",
+          tabBarInactiveTintColor: "#dcdcdc",
         })}
-        tabBarOptions={{
-          activeTintColor: "red",
-          inactiveTintColor: "grey",
-          labelStyle: { paddingBottom: 10, fontSize: 10 },
-          style: { padding: 10, height: 70 },
-        }}
       >
         <Tab.Screen
+          name="Mplus"
+          component={HomeStackScreen}
           options={{ headerShown: false }}
-          name={homeName}
-          component={nuur}
         />
         <Tab.Screen
+          name="Хайх"
+          component={nuur1}
           options={{ headerShown: false }}
-          name={secondName}
+        />
+        <Tab.Screen
+          name="Миний сан"
           component={nuur2}
+          options={{ headerShown: false }}
         />
         <Tab.Screen
-          options={{ headerShown: false }}
-          name={thirdName}
+          name="Message"
           component={nuur3}
-        />
-        <Tab.Screen
           options={{ headerShown: false }}
-          name={fourName}
-          component={nuur4}
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-export default MainContainer;
+export default MyTabs;
